0657a9133872fe6a99f30dba9da358eeea14ca8d
boussad <bsd@>
2014-02-11 14:01:51 +0100

    res

--- projTestTrain1/src/pack1/C1.java
+++ projTestTrain1/src/pack1/C1.java
@@ -11,7 +11,7 @@ public class C1 {
 		
 		System.out.println(" C1 :  Hello world ");
 		C2.showMssg(); 
-	   System.out.println(" Hello  man !  how are  you  ");	
+	   System.out.println(" Hello  man !  how are  you ");	
 		System.out.println("  Hellow toto ");
 
 	}
