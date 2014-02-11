2789ee9c9b6a8611f9d89b647562382b78730caa
boussad <bsd@>
2014-02-11 11:32:15 +0100

    first commit

---
+++ projTestTrain1/src/pack1/C1.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class C1 {
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println(" C1 :  Hello world ");
+		C2.showMssg(); 
+	}
+
+}
---
+++ projTestTrain1/src/pack1/C2.java
@@ -0,0 +1,8 @@
+package pack1;
+
+public class C2 {
+
+	public static void  showMssg(){
+		  System.out.println("  hello c'est c2");
+	}
+}
