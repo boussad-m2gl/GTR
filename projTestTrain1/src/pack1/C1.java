96775187e4b4d05c7226e13d289ef39b8001035d
boussad <bsd@>
2014-02-11 13:59:18 +0100

    res conf

--- projTestTrain1/src/pack1/C1.java
+++ projTestTrain1/src/pack1/C1.java
@@ -10,8 +10,9 @@ public class C1 {
 	public static void main(String[] args) {
 		
 		System.out.println(" C1 :  Hello world ");
-		System.out.println("  Hellow toto ");
 		C2.showMssg(); 
+	   System.out.println(" Hello  man !  how are  you  ");	
+		System.out.println("  Hellow toto ");
 	}
 
 }
