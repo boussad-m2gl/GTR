7db4a2a94239cf55f056bca08b2c966e709d7c45
boussad <bsd@>
2014-03-10 18:59:38 +0100

    c1 cleaning

--- projTestTrain1/src/pack1/C1.java
+++ projTestTrain1/src/pack1/C1.java
@@ -3,17 +3,12 @@ package pack1;
 
 public class C1 {
 
-	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
-		System.out.println(" C1 :  Hello world ");
-		C2.showMssg(); 
-  	    System.out.println(" Hello  man !  how are  you ");	
-		System.out.println("  Hellow toto ");
-
+		   System.out.println("  C1, Its hardely clean now.");	
 	}
 
 }
